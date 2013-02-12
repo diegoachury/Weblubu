b46e6b4db8e0a801ec83703416997e6e054c59f6
diegoachury <tiran_o@hotmail.com>
2013-02-12 08:06:07 -0500

    escribo el codigo en el controlador para que me permita validar que se envio el mensaje correctamente

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,15 @@
 class MessagesController < ApplicationController
   def new
+  	@message = Message.new
   end
 
   def create
+  	@message = Message.new(params[:message])
+  	if @message.valid?
+  		#todo el envio del mensaje
+  		redirect_to root_url, notice:"mensaje enviado"
+  	else
+  		render "new"
+  	end 
   end
 end
