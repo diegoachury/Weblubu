9c8938f47488b489736d018f25bb0ba880f1254e
diegoachury <tiran_o@hotmail.com>
2013-02-12 10:45:23 -0500

    migro la base de datos, creo un archivo llamado setup_mail.rb dentro de la carpeta config/initializers creo la conexion de envio de correos

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.smtp_settings ={
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:domain => "lubu.com.co",
+	:user_name => "dachury@lubu.com.co",
+	:password => "angelnegro",
+	:authentication => "plain",
+	:enable_starttls_auto => true
+
+}
