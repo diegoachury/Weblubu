ef4ded7a530aeb1f83942994ec1aeab85f34662f
diegoachury <tiran_o@hotmail.com>
2013-02-15 17:59:57 -0500

    subiendo cambios de plantilla de enviar correo y informacion del correo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "info@lubu.com.co"
   def reply_contact(contact)
-  	mail(:to =>contact.email, :subject =>"registro")
+  	mail(:to =>contact.email, :subject =>"soluciones Lubu SAS")
   end
 
 end
